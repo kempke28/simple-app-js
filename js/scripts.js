a338d0dbfd028bb4e120c51de72e2e13238bb018
Akempke <akempkedev@gmail.com>
2021-04-09 00:11:47 +0100

    Main pokemons aadded
    Great jooob! Primtive and complex data

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,36 @@
-alert('Hello worid!')
+alert('Gooo Pikachu!!')
 
-let favoriteFood = 'sushi';  //first variable on my js
-document.write(favoriteFood);
+let pokemonListe = [
+    {
+        name: 'Bulbasaur',
+        typ: ['grass', 'posion'],
+        evolution: 'ivysaur',
+        niveau_evolution: 16,
+        beschreibung: 'For some time after its birth, it grows by gaining nourishment from the seed on its back',
+        schwach_gegen: ['ground', 'psychic', 'flying', 'fire', 'ice']
+    }, 
+    {
+        name: 'Charmander',
+        typ: 'fire',
+        evolution: 'Charmeleon',
+        niveau_evolution: 16,
+        beschreibung: 'The fire on the tip of its tail is a measure of its life. If healthy, its tail burns intensely.',
+        schwach_gegen: ['ground', 'rock', 'water']
+    },
+    {
+        name: 'Squirtle',
+        typ: 'water',
+        evolution: 'Wartortle',
+        niveau_evolution: 16,
+        beschreibung: 'It is said to live 10,000 years. Its furry tail is popular as a symbol of longevity.'
+        schwach_gegen: ['grass', 'electric']
+    }
+    {
+        name: 'Pikachu',
+        typ: 'electric',
+        evolution: 'Raichu',
+        niveau_evolution: 'using a stone',
+        beschreibung: 'It occasionally uses an electric shock to recharge a fellow Pikachu that is in a weakened state.',
+        schwach_gegen: 'ground'
+    }
+]
